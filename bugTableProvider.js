4a89a3a91582ffd75ab0dfb9b0bb7ed697eb57e5
1000copy <1000copy@gmail.com>
2020-10-16 12:06:47 +0800

    a working good table oid

--- bugTableProvider.js
+++ bugTableProvider.js
@@ -1,7 +1,10 @@
+var filename,modulename;
+filename = 'mib\\ex1.mib'
+modulename = 'NET-SNMP-EXAMPLES-MIB'
 var snmp = require ("net-snmp");
 var store = snmp.createModuleStore ();
-store.loadFromFile (__dirname+"\\tbittable.mib");
-var jsonModule = store.getModule ("tbitinc");
+store.loadFromFile (__dirname+"\\"+filename);
+var jsonModule = store.getModule (modulename);
 // console.log(jsonModule,__dirname)
-var providers = store.getProvidersForModule ("tbitinc");
+var providers = store.getProvidersForModule (modulename);
 console.log(providers)
